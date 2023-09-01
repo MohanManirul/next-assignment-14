a281eae368e6d96eaed892afcaa25318271c838d
MohanManirul <fiforeg@gmail.com>
2023-09-01 17:29:15 +0600

    middleware mechanism

--- app/api/utility/MiddlewareUtility.js
+++ app/api/utility/MiddlewareUtility.js
@@ -3,16 +3,17 @@ import { verifyToken } from "./JWTHelper";
 
 export async function CheckCookieAuth(req){
     try{
-        let token = req.cookies.get('token'); // how to see this token ?
+        let token = req.cookies.get('token'); // get the token named cookie
         console.log(token);
-        let payload = await verifyToken(token['value'])
+        let payload = await verifyToken(token['value']) // decript the cookie as plain text (original email)
         console.log(payload['email'])
-        const requestHeaders = new Headers(req.headers)
-        requestHeaders.set('email',payload['email'])
+        const requestHeaders = new Headers(req.headers) // verify the req.headers (token) regularly
+        requestHeaders.set('email',payload['email']) // set the payload['email'] at header
+        // allow to go to controller
         return NextResponse.next({
             request:{headers:requestHeaders},
         })
     }catch(e){
-        return NextResponse.redirect(new URL('/login',req.url))
+        return NextResponse.redirect(new URL('/login',req.url)) // if not verified then deny to go to controller redirect to /login url
     }
 }
